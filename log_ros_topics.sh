140dd176812a79ef872bbe3cb501fd438a06baa2
tomqiluo <qiluo@andrew.cmu.edu>
2024-07-24 11:59:26 -0400

    log ros topics

--- log_ros_topics.sh
+++ log_ros_topics.sh
@@ -4,11 +4,11 @@
 mkdir -p ~/ros_logs
 
 # Start logging each topic to a separate file
-rostopic echo /diagnostic/planning_evaluator/metrics >> ~/ros_logs/planning_evaluator_metrics.log &
-rostopic echo /diagnostics >> ~/ros_logs/diagnostics.log &
-rostopic echo /diagnostics_agg >> ~/ros_logs/diagnostics_agg.log &
-rostopic echo /diagnostics_err >> ~/ros_logs/diagnostics_err.log &
-rostopic echo /diagnostics_toplevel_state >> ~/ros_logs/diagnostics_toplevel_state.log &
+ros2 topic echo /diagnostic/planning_evaluator/metrics >> ~/ros_logs/planning_evaluator_metrics.log &
+ros2 topic echo /diagnostics >> ~/ros_logs/diagnostics.log &
+ros2 topic echo /diagnostics_agg >> ~/ros_logs/diagnostics_agg.log &
+ros2 topic echo /diagnostics_err >> ~/ros_logs/diagnostics_err.log &
+ros2 topic echo /diagnostics_toplevel_state >> ~/ros_logs/diagnostics_toplevel_state.log &
 
 # Wait for any process to exit
 wait
